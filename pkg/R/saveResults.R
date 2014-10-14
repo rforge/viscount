e5a8e115263c48fa85c961135589089c966c794d
marcia <marcia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-14 18:01:45 +0000

    upgrade to version 1.1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/viscount@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/saveResults.R
+++ pkg/R/saveResults.R
@@ -1,8 +1,8 @@
 saveResults <- function(path = "VisCountSessions.csv") {
   if (exists("VisCountSessions")) {
     write.csv(VisCountSessions, path, row.names = FALSE)
-    message("Results saved at '", getwd(), "/VisCountSessions.csv'.\nRe-start R if you want to start a new VisCount session, or keep going\nif you want results added to the current session.")
+    message("Results saved at '", getwd(), "/VisCountSessions.csv'.\nRe-start R if you want to start a new visCount session, or keep going\nif you want results added to the current session.")
   } else {
-    message ("No VisCountSessions results to save. Use VisCount() a few times first,\nproviding some count estimates.")
+    message ("No VisCountSessions results to save. Use visCount() a few times first,\nproviding some count estimates.")
   }
 }
